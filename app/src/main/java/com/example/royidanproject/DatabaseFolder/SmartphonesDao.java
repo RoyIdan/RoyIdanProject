e4c0c8e4ae33bf9d96cf83bb462500df099f79fe
רועי עידן <S32334@nachshon.local>
2021-12-14 08:27:00 +0200

    Initial commit

---
+++ app/src/main/java/com/example/royidanproject/DatabaseFolder/SmartphonesDao.java
@@ -0,0 +1,21 @@
+package com.example.royidanproject.DatabaseFolder;
+
+import java.util.List;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+@Dao
+public interface SmartphonesDao {
+
+    @Query("SELECT * FROM tblSmartphones WHERE productId = :smartphoneId")
+    Smartphone getSmartphoneById(long smartphoneId);
+
+    @Insert
+    long insert(Smartphone smartphone);
+
+    @Query("SELECT * FROM tblsmartphones WHERE :query")
+    List<Smartphone> getByQuery(String query);
+
+}
