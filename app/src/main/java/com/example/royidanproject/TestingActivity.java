0b3a0818689b8bbb8f48a02e83ab2ea3086bbdd4
s32334 <royidan1@gmail.com>
2022-02-24 21:06:00 +0200

    24/2 idk what am i doing anymore

--- app/src/main/java/com/example/royidanproject/TestingActivity.java
+++ app/src/main/java/com/example/royidanproject/TestingActivity.java
@@ -1,12 +1,13 @@
 package com.example.royidanproject;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
 
-import com.example.royidanproject.Views.CreditCardView;
 
 import java.util.Date;
 
@@ -15,13 +16,17 @@ public class TestingActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        LinearLayout llRoot = findViewById(R.id.llRoot);
+        LayoutInflater.from(TestingActivity.this).inflate(R.layout.custom_toolbar, llRoot);
         setContentView(R.layout.activity_testing);
 
-        LinearLayout llRoot = findViewById(R.id.llRoot);
 
-        CreditCardView ccv = new CreditCardView(TestingActivity.this, null);
-        ccv.setCardExpireDate(new Date());
 
-        llRoot.addView(ccv);
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.custom_toolbar);
+        getSupportActionBar().hide();
+
+
+
     }
 }
